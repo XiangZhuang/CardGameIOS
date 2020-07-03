6cf14db655e248dc5c97c4d034602842ef1ec83e
ZHUANG <xzhuangaa@lbe028.ust.hk>
2020-07-03 18:25:33 +0800

    Set up

---
+++ FirstApp/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  FirstApp
+//
+//  Created by ZHUANG, Xiang on 3/7/2020.
+//  Copyright © 2020 ZHUANG, Xiang. All rights reserved.
+//
+
+import Foundation
---
+++ FirstApp/CardCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CardCollectionViewCell.swift
+//  FirstApp
+//
+//  Created by ZHUANG, Xiang on 3/7/2020.
+//  Copyright © 2020 ZHUANG, Xiang. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+}
